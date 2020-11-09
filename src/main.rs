98c9c495963e9f2739a671e5b9d29b0a19f94c47
Wei Liu <sunnywhy@gmail.com>
2020-11-08 23:55:46 -0500

    use random port for testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our server
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,12 @@
+use std::net::TcpListener;
+
 // `actix_rt::test` is the testing equivalent of `actix_web:main`.
 // It also spares you from having to specify the `#[test]` attribute.
 // You can inspect the code by using `cargo expand --test health_check` (<-name of the test file)
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     // We brought `reqwest` in as a _development _dependency
     // to perform HTTP requests against our application.
     // Either add it manually under [dev-dependencies] in Cargo.toml
@@ -13,7 +15,7 @@ async fn health_check_works() {
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute reqwest.");
@@ -25,13 +27,18 @@ async fn health_check_works() {
 
 // Launch our application in the backend.
 // This is the only piece that will, reasonably, depend on our application code.
-fn spawn_app() {
+fn spawn_app() -> String {
     // New dev dependency - let's add tokio to the party with
     // `cargo add tokio --dev --vers 0.2.22`
-    let server = zero2prod::run().expect("Failed to bind address");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
 
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it, hence the non-binding let
     let _ = tokio::spawn(server);
+
+    // return the application address to the caller
+    format!("http://127.0.0.1:{}", port)
 }
